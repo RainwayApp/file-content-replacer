2ae86c24da28377693ba61d13954c55c4dadc4ec
Ralph Shillington <ralph.shillington@feenics.com>
2020-01-09 15:42:12 -0500

    now using replace-in-file npm package

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 
 const core = require('@actions/core');
-const fs = require('fs');
+const replace = require('replace-in-file');
 
 async function run() {
     try {
-        const version = core.getInput('version')
-        const filename = core.getInput('filename')
-        try {
-            console.log(`Looking for file ${filename}`);
-            await fs.promises.access(filename, fs.constants.F_OK | fs.constants.W_OK);
+        const from = core.getInput('from', { required: true });
+        const to = core.getInput('to', { required: true });
+        const files = core.getInput('files', { required: true });
+        const options = { files, from: new RegExp(from), to };
 
-            console.log(`found file ${filename} and now setting version ${version}`);
-            try {
-                let content = await fs.promises.readFile(filename, "utf-8");
-                content=content.replace(/\[assembly:\s*AssemblyVersion\(".*"\)]/g,`[assembly: AssemblyVersion("${version}")]`)
-                    .replace(/\[assembly:\s*AssemblyFileVersion\(".*"\)]/g,`[assembly: AssemblyFileVersion("${version}")]`);
-                
-                await fs.promises.writeFile(filename,content);
-                
-                 
-            } catch (err) {
-                core.setFailed(err.message);
-            }
-        } catch (err) {
+        const results = await replace(options);
+        console.log(results);
 
-            core.setFailed(err.message)
-        }
     } catch (error) {
 
         core.setFailed(error.message)
